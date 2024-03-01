f740a480ea1e0bbf3cbf458933aa83905a5c6cad
ProgrammerLP <developer.programmerlp@gmx.net>
2024-03-01 20:38:06 +0100

    .

--- ActivityPulse/Settings/AppSettings.Designer.cs
+++ ActivityPulse/Settings/AppSettings.Designer.cs
@@ -25,7 +25,7 @@ namespace ActivityPulse.Settings {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public int ThemeMode {
             get {
                 return ((int)(this["ThemeMode"]));

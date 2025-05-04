f8b9383066ebf81cce24cc6c4171cd408e560195
ProgrammerLP <developer.programmerlp@gmx.net>
2025-05-04 22:09:51 +0200

    add & edit categories

--- ActivityPulse/Pages/RemindersPage.xaml.cs
+++ ActivityPulse/Pages/RemindersPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using ActivityUtils.Data;
+using ActivityUtils.Models;
+using System.Windows.Controls;
 
 namespace ActivityPulse.Pages
 {
@@ -7,23 +9,66 @@ namespace ActivityPulse.Pages
     /// </summary>
     public partial class RemindersPage : Page
     {
+        List<ReminderContext> reminders;
+        List<CategoryContext> categories;
+
         public RemindersPage()
         {
             InitializeComponent();
             MainWindow.UpdateTitle("ActivityPulse - Reminders");
+            categories = DataReminders.LoadCategories();
+            lbCategories.ItemsSource = categories;
+            lbReminders.ItemsSource = reminders;
         }
 
         private void addCategoryBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            categoryDialog.Title = "Add category";
+            categoryDialog.Open();
         }
 
         private void allCategoriesBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            lbCategories.SelectedIndex = -1;
         }
 
         private void lbCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbCategories.SelectedIndex >= 0)
+            {
+
+            }
+        }
+
+        private void categoryDialog_OnCategoryDialogClick(CategoryContext category)
+        {
+            if (!categoryDialog.IsEditMode)
+            {
+                categories.Add(category);
+            }
+            else
+            {
+                if (lbCategories.SelectedIndex >= 0)
+                {
+                    if (categories[lbCategories.SelectedIndex].Id == category.Id)
+                    {
+                        categories[lbCategories.SelectedIndex] = category;
+                    }
+                }
+            }
+            lbCategories.ItemsSource = null;
+            lbCategories.ItemsSource = categories;
+            DataReminders.SaveCategories(categories);
+        }
+
+        private void miEdit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            categoryDialog.Title = "Edit category";
+            categoryDialog.EditCategory = (CategoryContext)lbCategories.SelectedItem;
+            categoryDialog.Open();
+        }
+
+        private void miDelete_Click(object sender, System.Windows.RoutedEventArgs e)
         {
 
         }
---
+++ ActivityPulse/UserControls/ManageCategoryControl.xaml.cs
@@ -0,0 +1,103 @@
+﻿using ActivityPulse.Windows;
+using ActivityUtils.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace ActivityPulse.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für ManageCategoryControl.xaml
+    /// </summary>
+
+    public delegate void CategoryDialogClick(CategoryContext category);
+    public partial class ManageCategoryControl : UserControl
+    {
+        public event CategoryDialogClick OnCategoryDialogClick;
+        public string Title { get => tbTitle.Text; set => tbTitle.Text = value; }
+        bool isEditMode;
+        public bool IsEditMode { get => isEditMode; }
+        CategoryContext editCategory;
+        public CategoryContext EditCategory
+        {
+            set
+            {
+                editCategory = value;
+                isEditMode = true;
+                tbName.Text = value.Name;
+            }
+            get { return editCategory; }
+        }
+
+        public ManageCategoryControl()
+        {
+            InitializeComponent();
+        }
+
+        private void closeDialog_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (tbName.Text.Length > 0)
+                {
+                    if (!isEditMode)
+                    {
+                        OnCategoryDialogClick(new CategoryContext(tbName.Text));
+                    }
+                    else
+                    {
+                        editCategory.Name = tbName.Text;
+                        OnCategoryDialogClick(editCategory);
+                        isEditMode = false;
+                    }
+                    tbName.Text = "";
+                    Close();
+                }
+                else
+                {
+                    TellBox tb = new TellBox("Please enter a name!", "Error");
+                    tb.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                TellBox tellBox = new TellBox(ex.Message, "Error");
+            }
+        }
+
+        public void Open()
+        {
+            //größer
+            Visibility = Visibility.Visible;
+            BeginStoryboard bg1 = this.FindResource("BGOPEN") as BeginStoryboard;
+            bg1.Storyboard.Begin();
+
+            BeginStoryboard db250 = this.FindResource("DWOPEN") as BeginStoryboard;
+            db250.Storyboard.Begin();
+
+            BeginStoryboard dh300 = this.FindResource("DHOPEN") as BeginStoryboard;
+            dh300.Storyboard.Begin();
+        }
+
+        public void Close()
+        {
+            BeginStoryboard db0 = this.FindResource("DWCLOSE") as BeginStoryboard;
+            db0.Storyboard.Begin();
+
+            BeginStoryboard dh0 = this.FindResource("DHCLOSE") as BeginStoryboard;
+            dh0.Storyboard.Begin();
+
+            BeginStoryboard bg0 = this.FindResource("BGCLOSE") as BeginStoryboard;
+            bg0.Storyboard.Completed += new EventHandler(delegate (object sender, EventArgs e)
+            {
+                Visibility = Visibility.Collapsed;
+            });
+            bg0.Storyboard.Begin();
+        }
+    }
+}

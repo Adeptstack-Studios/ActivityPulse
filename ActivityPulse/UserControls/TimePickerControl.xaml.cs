735cb16e5c399f94fb625002dcea1ee90ee93753
ProgrammerLP <developer.programmerlp@gmx.net>
2025-05-08 22:15:06 +0200

    added CountBoxControl and TimePickerControl for ReminderDialog

---
+++ ActivityPulse/UserControls/TimePickerControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ActivityPulse.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für TimePickerControl.xaml
+    /// </summary>
+    public partial class TimePickerControl : UserControl
+    {
+        public TimePickerControl()
+        {
+            InitializeComponent();
+        }
+
+        private void tbTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnPopup_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = !popup.IsOpen;
+        }
+    }
+}
